b2f937d607e7e9a916b9e957a630225a294e25ae
liorosenman <lros209@gmail.com>
2024-06-13 17:45:19 +0300

    crud_for_customers_done

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ from helpers import create_db_tables
 from flask import Flask, render_template, request, redirect, url_for, session
 import db_orders
 
+import sqlite3
+con = sqlite3.connect("library.db",check_same_thread=False)
+cur = con.cursor()
+
+try:
+    cur.execute("CREATE TABLE customers(name,city,age)")
+except:
+    print("table already exist")
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
@@ -21,6 +30,7 @@ def login():
             return render_template('customers.html', customers = customers)
         else:
             return "Only Admin"
+    
         
 
 @app.route('/add_customer')
@@ -48,6 +58,22 @@ def new_customer():
         conn.close()
         return render_template('customers.html', customers = customers)
     return render_template('customers.html')
+
+@app.route('/upd/<int:id>',methods=['put'])
+def upd_user(id):
+    data = request.json
+    newName = data.get('name')
+    newCity = data.get('city')
+    newAge = data.get('age')
+    sql =f"UPDATE customers SET name = '{newName}', city='{newCity}', age={newAge} WHERE  rowid = {id}"
+    cur.execute(sql)
+    con.commit()
+
+@app.route('/del/<int:id>', methods = ['delete'])
+def del_user(id):
+    sql = f"DELETE FROM customers WHERE rowid = {id}"
+    cur.execute(sql)
+    con.commit()
     
 @app.route('/books')
 def books():
